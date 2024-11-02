b537f318d94a20e70aed281303d7a57e694375dc
Faisalkhan45 <faisalkhanattari45@gmail.com>
2024-11-02 17:02:59 +0530

    UBUNTU_WEBSITE.sh

--- UBUNTU_WEBSITE.sh
+++ UBUNTU_WEBSITE.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-sudo su
 
-apt update
-apt install unzip apache2 wget -y
+# Update package lists
+sudo apt update
 
-systemctl start apache2
-systemctl enable apache2
+# Install necessary packages
+sudo apt install unzip apache2 wget -y
 
+# Start and enable Apache
+sudo systemctl start apache2
+sudo systemctl enable apache2
+
+# Download and unzip the website template
 wget https://www.free-css.com/assets/files/free-css-templates/download/page296/neogym.zip
-unzip -o neogym.zip
+sudo unzip -o neogym.zip -d /tmp  # Unzip to a temporary directory
 
-cp -r neogym-html/* /var/www/html
-systemctl restart apache2
+# Copy the files to the web directory
+sudo cp -r /tmp/neogym-html/* /var/www/html
+
+# Restart Apache to apply changes
+sudo systemctl restart apache2
+
